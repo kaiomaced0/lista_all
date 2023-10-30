cf60dc50a57049454247357e4dc36fb65c21f7d1
Kaio Macedo <kaiomm2000@gmail.com>
2023-10-30 15:02:41 -0300

    .

--- lib/repositories/carrinho_repository.dart
+++ lib/repositories/carrinho_repository.dart
@@ -4,27 +4,27 @@ import 'package:lista_all/model/carrinho.dart';
 import 'package:lista_all/repositories/item_repository.dart';
 import 'package:lista_all/repositories/usuario_repository.dart';
 
-class CarrinhoRepository {
-  static void adicionaLista(int posicao, Item item) {
-    CarrinhoRepository.carrinhos[posicao].itensCarrinho.add(item);
-    calculaPrecoLista(posicao);
-  }
-
-  static void calculaPrecoLista(int posicao) {
-    double a = 0.0;
-    for (var i = 0;
-        i < CarrinhoRepository.carrinhos[posicao].itensCarrinho.length;
-        i++) {
-      a = CarrinhoRepository.carrinhos[posicao].itensCarrinho[i].preco + a;
+  class CarrinhoRepository {
+    static void adicionaLista(int posicao, Item item) {
+      CarrinhoRepository.carrinhos[posicao].itensCarrinho.add(item);
+      calculaPrecoLista(posicao);
     }
-    CarrinhoRepository.carrinhos[posicao].valorTotal = a;
-  }
 
-  static void removeLista(int posicao, Item item) {
-    CarrinhoRepository.carrinhos[posicao].itensCarrinho.remove(item);
-    calculaPrecoLista(posicao);
-    
-  }
+    static void calculaPrecoLista(int posicao) {
+      double a = 0.0;
+      for (var i = 0;
+          i < CarrinhoRepository.carrinhos[posicao].itensCarrinho.length;
+          i++) {
+        a = CarrinhoRepository.carrinhos[posicao].itensCarrinho[i].preco + a;
+      }
+      CarrinhoRepository.carrinhos[posicao].valorTotal = a;
+    }
+
+    static void removeLista(int posicao, Item item) {
+      CarrinhoRepository.carrinhos[posicao].itensCarrinho.remove(item);
+      calculaPrecoLista(posicao);
+      
+    }
 
   static List<Item> itensVazio2 = [
     ItemRepository.tabela[0],
